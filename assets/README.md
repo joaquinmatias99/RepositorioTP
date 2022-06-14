048a7764e502edb499abcf4734fc7128f7702050
joaquin muñoz <92613299+joaquinmatias99@users.noreply.github.com>
2022-06-13 23:20:11 -0300

    Add files via upload

---
+++ assets/README.md
@@ -0,0 +1,16 @@
+# Buquealtoque.com | UNGS
+
+## Materia
+Especificación y verificación de Software COM 02
+
+## Profesores
+Cristian Ciarallo,
+Paula Perez Willis. 
+
+## Integrantes del grupo
+Matias Alpuin,
+Joaquin Zanardi,
+Joaquin Muñoz.
+
+## Descripción
+Proyecto para Trabajo Práctico "BuqueAltoque"
---
+++ assets/assets/data-source/items.js
@@ -0,0 +1,50 @@
+const items = [
+	{
+		name: 'Promo productos seleccionados',
+		price: '$500',
+		imageSrc: './assets/images/art1.jpg',
+		publicationType: 'Promocion FreeShop'
+	},
+	{
+		name: 'Hospedaje Hotel de Ibis + buque',
+		price: '$8.700',
+		imageSrc: './assets/images/art2.jpg',
+		publicationType: 'Paquete'
+	},
+	{
+		name: 'Hospedaje Hotel Nahuel Huapi + buque',
+		price: '$15.500',
+		imageSrc: './assets/images/art3.jpg',
+		publicationType: 'Paquete'
+	},
+	{
+		name: 'Hospedaje Hotel Donna + buque',
+		price: '$12.450',
+		imageSrc: './assets/images/art4.jpg',
+		publicationType: 'Paquete'
+	},
+	{
+		name: 'Buque hacia montevideo',
+		price: '$8.000',
+		imageSrc: './assets/images/art5.jpg',
+		publicationType: 'Pasaje'
+	},
+	{
+		name: 'Alojamiento Hotel Cambria + buque + servicios',
+		price: '$15.500',
+		imageSrc: './assets/images/art6.jpg',
+		publicationType: 'Paquete'
+	},
+	{
+		name: 'Alojamiento hotel Hilton + buque + comida',
+		price: '$13.200',
+		imageSrc: './assets/images/art7.jpg',
+		publicationType: 'Paquete'
+	},
+	{
+		name: 'Buque atlanta + servicios',
+		price: '$15.000',
+		imageSrc: './assets/images/art8.jpg',
+		publicationType: 'Daytoor'
+	}
+];
